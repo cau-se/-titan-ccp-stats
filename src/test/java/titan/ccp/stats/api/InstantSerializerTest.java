8753ac70bfc1b65c83e21d3e609eb092e6216ffa
Simon Ehrenstein <simon.ehrenstein@gmail.com>
2019-10-17 14:30:47 +0200

    Add unit tests for gson serializers

--- src/test/java/titan/ccp/stats/api/InstantSerializerTest.java
+++ src/test/java/titan/ccp/stats/api/InstantSerializerTest.java
@@ -1,13 +1,30 @@
 package titan.ccp.stats.api;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonPrimitive;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
 import org.junit.Test;
+import titan.ccp.stats.api.util.InstantSerializer;
 
 public class InstantSerializerTest {
 
+  private final Gson gson = new GsonBuilder()
+      .registerTypeAdapter(Instant.class, new InstantSerializer())
+      .create();
+
   @Test
   public void test() {
-    fail("Not yet implemented");
+    final Instant instant = Instant.ofEpochMilli(1L);
+
+    final String serializedInstant = this.gson.toJson(instant);
+
+    final String expectedSerializedInstant =
+        new JsonPrimitive(DateTimeFormatter.ISO_INSTANT.format(instant)).toString();
+
+    assertEquals(serializedInstant, expectedSerializedInstant);
   }
 
 }
--- src/test/java/titan/ccp/stats/api/IntervalSerializerTest.java
+++ src/test/java/titan/ccp/stats/api/IntervalSerializerTest.java
@@ -1,13 +1,43 @@
 package titan.ccp.stats.api;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
 import org.junit.Test;
+import titan.ccp.stats.api.util.InstantSerializer;
+import titan.ccp.stats.api.util.Interval;
+import titan.ccp.stats.api.util.IntervalSerializer;
 
 public class IntervalSerializerTest {
 
+  private final Gson gson = new GsonBuilder()
+      .registerTypeAdapter(Instant.class, new InstantSerializer())
+      .registerTypeAdapter(Interval.class, new IntervalSerializer())
+      .create();
+
   @Test
   public void test() {
-    fail("Not yet implemented");
+    final Instant start = Instant.ofEpochMilli(0L);
+    final Instant end = Instant.ofEpochMilli(1L);
+
+    final Interval interval = Interval.of(start, end);
+
+    final String serializedInterval = this.gson.toJson(interval);
+
+    final JsonElement expectedStart =
+        new JsonPrimitive(DateTimeFormatter.ISO_INSTANT.format(start));
+    final JsonElement expectedEnd =
+        new JsonPrimitive(DateTimeFormatter.ISO_INSTANT.format(end));
+    final JsonObject expectedInterval = new JsonObject();
+    expectedInterval.add("intervalStart", expectedStart);
+    expectedInterval.add("intervalEnd", expectedEnd);
+
+    assertEquals(serializedInterval, expectedInterval.toString());
   }
 
 }
