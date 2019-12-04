0acd31d723ceabb646e840ca4d5f02aca25c8ed4
Björn Vonheiden <bjoern.vonheiden@hotmail.de>
2019-12-04 11:23:31 +0100

    remove GenericSerde and use it from commons

--- src/main/java/titan/ccp/stats/streamprocessing/Serdes.java
+++ src/main/java/titan/ccp/stats/streamprocessing/Serdes.java
@@ -2,13 +2,13 @@ package titan.ccp.stats.streamprocessing;
 
 import com.google.common.math.Stats;
 import org.apache.kafka.common.serialization.Serde;
+import titan.ccp.common.kafka.simpleserdes.GenericSerde;
 import titan.ccp.common.kieker.kafka.IMonitoringRecordSerde;
 import titan.ccp.model.records.WindowedActivePowerRecord;
 import titan.ccp.models.records.ActivePowerRecord;
 import titan.ccp.models.records.ActivePowerRecordFactory;
 import titan.ccp.models.records.AggregatedActivePowerRecord;
 import titan.ccp.models.records.AggregatedActivePowerRecordFactory;
-import titan.ccp.stats.streamprocessing.util.GenericSerde;
 import titan.ccp.stats.streamprocessing.util.SchemaRegistryAvroSerdeFactory;
 
 final class Serdes {
